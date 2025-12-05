bc389f64689cba79658cbb4789db2876813cdf26
Jackson Darrow <JuicyPasta@users.noreply.github.com>
2025-12-05 21:22:29 +0000

    day 2 part 2 progress

--- day1/day1.cc
+++ day1/day1.cc
@@ -4,7 +4,9 @@
 
 using namespace std;
 
-int main () {
+
+
+int day1_part1() {
     freopen("input.txt", "r", stdin);
 
     int pos = 50; 
@@ -26,8 +28,31 @@ int main () {
         if (pos < 0) {
             pos = 100+pos;
         }
+        if (pos == 0) zero_count++;
 
+    }
 
+    fclose(stdin);
+    return zero_count;    
+}
+
+int day1_part2() {
+    freopen("input.txt", "r", stdin);
+
+    int pos = 50; 
+    int zero_count = 0;
+    std::string line;
+
+    while (cin >> line) {
+        int times = stoi(line.substr(1, line.size()));
+        line[0] == 'L' ? pos -= times : pos += times;
+
+        zero_count += pos / 100;
+
+        pos = pos % 100;
+        if (pos < 0) {
+            pos = 100+pos;
+        }
         cout << "Read line: " << line << " times " << times <<  " New Pos: " << pos << endl;
 
         if (pos == 0) zero_count++;
@@ -35,4 +60,14 @@ int main () {
     }    
 
     cout << "Zero Count: " << zero_count << endl;
+    fclose(stdin);
+    return zero_count;
+}
+
+int main () {
+    //int day1_part1_out = day1_part1();
+    //cout << "Day 1 part 1: " << day1_part1_out << endl;
+
+    int day1_part2_out = day1_part2();
+    cout << "Day 1 part 2: " << day1_part2_out << endl;
 }
